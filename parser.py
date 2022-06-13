cdc8cb2767f91b822730ff94b0c9e669e65adb70
folet14 <jed14@orange.fr>
2022-06-13 19:39:56 +0200

    new parser moving

--- parser.py
+++ parser.py
@@ -1,64 +1,230 @@
 import re
 from unicodedata import category
 import xml.etree.ElementTree as ET
+import jpype
+import jaydebeapi
 
 class questionClass:
-    def __init__(self, quest):
+    def __init__(self, type, quest):
+        self.single = None
+        self.feedback = None
+        self.type = type
         self.quest = quest
         self.answers = []
 
     def addAnswer(self, answer, fraction):
-        self.answers.append((answer,fraction))
+        self.answers.append((answer, fraction))
 
-    def setFeedback(self, general, correct, partial, incorrect):
-        self.feedback = (general, correct, partial, incorrect)
+    def setFeedback(self, general="", correct="", partial="", incorrect=""):
+        self.feedback = [general, correct, partial, incorrect]
+
+    def setSingle(self, value):
+        self.single = value
 
     def getFeedback(self):
         return self.feedback
 
+    def getType(self):
+        return self.type
+
     def getQuestion(self):
         return self.quest
-        
+
     def getAnswers(self):
         return self.answers
 
+    def getSingle(self):
+        return self.single
+
+
 def XMLparser(filename):
+    def removeThingy(text):
+        HTMLTag = re.compile('<.*?>')
+        insecTag = re.compile('-?&nbsp;?')
+        text = re.sub(HTMLTag, '', str(text))
+        text = re.sub(insecTag, '', str(text))
+        return text
+
+
     tree = ET.parse(filename)
     root = tree.getroot()
     if root.tag != "quiz":
         print("wrong xml file, not a quiz")
-        exit
-
-    HTMLTag = re.compile('<.*?>') 
+        exit()
 
     questionList = []
     for question in root:
-        type = question.attrib.get("type")
-        if type == "category":
+        currentType = question.attrib.get("type")
+        if currentType == "category":
             pass
-        elif type == "multichoice" or type == "truefalse":
+
+        elif currentType == "multichoice":
             for child in question:
                 if child.tag == "name":
                     pass
-                elif child.tag == "questiontext":
-                    newQuestion = questionClass(re.sub(HTMLTag, '', str(child[0].text)))
                 elif child.tag == "generalfeedback":
-                    general = re.sub(HTMLTag, '', str(child[0].text))
+                    general = removeThingy(child[0].text)
+                elif child.tag == "questiontext":
+                    newQuestion = questionClass(currentType, removeThingy(child[0].text))
+                elif child.tag == "defaultgrade":
+                    pass
+                elif child.tag == "penalty":
+                    pass
+                elif child.tag == "hidden":
+                    pass
+                elif child.tag == "idnumber":
+                    pass
+                elif child.tag == "single":
+                    single = child.text
+                elif child.tag == "shuffleanswers":
+                    pass
+                elif child.tag == "answernumbering":
+                    pass
+                elif child.tag == "showstandardinstruction":
+                    pass
                 elif child.tag == "correctfeedback":
-                    correct = re.sub(HTMLTag, '', str(child[0].text))
+                    correct = removeThingy(child[0].text)
                 elif child.tag == "partiallycorrectfeedback":
-                    partial = re.sub(HTMLTag, '', str(child[0].text))
+                    partial = removeThingy(child[0].text)
                 elif child.tag == "incorrectfeedback":
-                    incorrect = re.sub(HTMLTag, '', str(child[0].text))
+                    incorrect = removeThingy(child[0].text)
                 elif child.tag == "answer":
                     fraction = child.attrib.get("fraction")
-                    newQuestion.addAnswer(re.sub(HTMLTag, '', str(child[0].text)),fraction)
+                    newQuestion.addAnswer(
+                        removeThingy(child[0].text), fraction)
             newQuestion.setFeedback(general, correct, partial, incorrect)
+            newQuestion.setSingle(single)
             questionList.append(newQuestion)
-        # elif type == "truefalse":
-        #     for child in question:
-        #         if child.tag == "name":
-        #             for sub in child:
-        #                 pass
+
+        elif currentType == "truefalse":
+            for child in question:
+                if child.tag == "name":
+                    pass
+                elif child.tag == "generalfeedback":
+                    general = removeThingy(child[0].text)
+                elif child.tag == "questiontext":
+                    newQuestion = questionClass(
+                        currentType, removeThingy(child[0].text))
+                ##elif child.tag == "defaultgrade":
+                ##    pass
+                ##elif child.tag == "penalty":
+                ##    pass
+                ##elif child.tag == "hidden":
+                ##    pass
+                ##elif child.tag == "idnumber":
+                ##    pass
+                ##elif child.tag == "shuffleanswers":
+                ##    pass
+                ##elif child.tag == "answernumbering":
+                ##    pass
+                ##elif child.tag == "showstandardinstruction":
+                ##    pass
+                elif child.tag == "correctfeedback":
+                    correct = removeThingy(child[0].text)
+                elif child.tag == "incorrectfeedback":
+                    incorrect = removeThingy(child[0].text)
+                elif child.tag == "answer":
+                    fraction = child.attrib.get("fraction")
+                    newQuestion.addAnswer(
+                        removeThingy(child[0].text), fraction)
+            newQuestion.setFeedback(general = general,correct =  correct, incorrect = incorrect)
+            questionList.append(newQuestion)
 
     return questionList
+
+def Upload(questionList):
+    JHOME = jpype.getDefaultJVMPath()
+    jpype.startJVM(classpath=['jar/ojdbc6.jar'])
+    con = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver',
+                            'jdbc:oracle:thin:legoffad/47671b85b1@im2ag-oracle.e.ujf-grenoble.fr:1521:im2ag')
+    cur = con.cursor()
+    nbQst = 0
+    for question in questionList:
+        nbQst+=1
+        enonce = question.getQuestion()
+        reponses = question.getAnswers()
+        general = question.getFeedback()[0]
+        nbRep=0
+        for i in reponses:
+            nbRep+=1
+
+        if(nbRep == 2):
+            #Question vrai/faux
+            err = 0
+            if(reponses[0][0] == reponses[1][0]):
+                err = 1
+                print("probleme de reponses dupliquees pour la question "+enonce)
+            if err == 0:
+                stmt = "INSERT INTO Questions VALUES(NULL, 1, 'None','"+enonce+"','"+general+"')"
+                try:
+                    cur.execute(stmt)
+                    print("inserted :"+stmt)
+                except jaydebeapi.DatabaseError as e:
+                    print("Couldn't exec request "+stmt)
+                    err = 1
+                if(err == 0):
+                    stmt ="SELECT ID FROM Questions WHERE question='"+enonce+"'"
+                    cur.execute(stmt)
+
+                    for i in cur.fetchall():
+                        id = i[0]
+
+                    for rep in reponses:
+                        if int(rep[1]) == 100:
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[1]+"')"
+                        elif int(rep[1]) == 0:
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[3]+"')"
+                        else:
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[2]+"')"
+
+                        try:
+                            cur.execute(stmt)
+                            print("inserted :"+stmt)
+                        except jaydebeapi.DatabaseError as e:
+                            print("Couldn't exec request "+stmt)
+        else:
+            #Question QCM avec 1 ou plusieurs reponses juste
+            nbRepJuste = 0
+            err = 0
+            previous = []
+            for rep in reponses:
+
+                for p in previous :
+                    if(p[0] == rep[0]):
+                        print("probleme de reponses dupliquees pour la question "+enonce)
+                        err = 1
+                previous.append(rep)
+
+                if(0<int(rep[1])<100):
+                    nbRepJuste = 2
+
+            if(err ==  0):
+                stmt = "INSERT INTO Questions VALUES(NULL, "+str(nbRepJuste)+", 'None','"+enonce+"','"+general+"')"
+                try:
+                    cur.execute(stmt)
+                    print("inserted :"+stmt)
+
+                except Exception as ex:
+                    print("Couldn't exec request "+stmt)
+                    err = 1
+
+                if err == 0:
+
+                    stmt = "SELECT ID FROM Questions WHERE question='"+enonce+"'"
+                    cur.execute(stmt)
+
+                    for i in cur.fetchall():
+                        id = i[0]
+
+                    for rep in reponses:
+                        if int(rep[1]) == 100:
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[1]+"')"
+                        elif int(rep[1]) == 0 :
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[3]+"')"
+                        else:
+                            stmt = "INSERT INTO Reponses VALUES("+str(id)+", '"+rep[0]+"', "+rep[1]+",'"+question.getFeedback()[2]+"')"
+                        try:
+                            cur.execute(stmt)
+                            print("inserted :"+stmt)
+                        except jaydebeapi.DatabaseError as e:
+                            print("Couldn't exec request "+stmt)
--- visualParser.py
+++ visualParser.py
@@ -1,77 +1,339 @@
-import copy
 import parser as p
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 
 
+##---------------------------unique window------------------------------------
 class visualParser(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+  def __init__(self, *args, **kwargs):
+    tk.Tk.__init__(self, *args, **kwargs)
+    container = tk.Frame(self)
+    self.title('Parser')
+    container.pack(side="top", fill="both", expand=True)
+    container.grid_rowconfigure(0, weight=1)
+    container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
-        self.questionList = []
-        self.curr = 0
-        self.nbQuest = 0
-        for F in (SelectionPage, CorrectionPage):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame("SelectionPage")
+    ##--------generating two frames------------
+    self.frames = {}
+    for F in (uploadPage, questionPage):  ##uploadPage for the xml import, question page to visualise and edit questions
+      page_name = F.__name__
+      frame = F(parent=container, controller=self)
+      self.frames[page_name] = frame
+      frame.grid(row=0, column=0, sticky="nsew")
+    self.show_frame("uploadPage")
 
-    def setQuestionList(self, list):
-        self.questionList = copy.deepcopy(list)
+  ##-------------unique window----------
+  def show_frame(self, page_name):
+    frame = self.frames[page_name]
+    frame.tkraise()
 
-    def popQuestionNb(self, nb):
-        if self.nbQuest > nb:
-            return self.questionList.pop(nb)
+  ##-------when xml is imported we initialise correctly the question page----------
+  def startQuestionPage(self, questionList):
+    questionPage = self.frames["questionPage"]
+    questionPage.setQuestionList(questionList)
+    questionPage.startEdition()
+    questionPage.tkraise()
 
-    def addQuestion(self, question):
-        self.questionList.append(question)
 
-    def changeNbQuest(self, nb):
-        self.nbQuest = nb
+##-----------------------------XML Frame definition------------------------------------
+class uploadPage(tk.Frame):
+  def __init__(self, parent, controller):
+    def UploadAction(questionList, event=None):
+      filename = filedialog.askopenfilename(filetypes=(("XML Files", "*.xml"),), title="XML file selection",
+                                            multiple=False)
+      print('Selected:', filename)
+      questionList = p.XMLparser(filename)
+      if questionList != []:
+        controller.startQuestionPage(questionList)
 
-    def show_frame(self, page_name):
-        frame = self.frames[page_name]
-        frame.tkraise()
+    tk.Frame.__init__(self, parent)
+    self.controller = controller
+    label = tk.Label(self, text="This is the upload page")
+    label.pack(side="top", fill="x", pady=10)
+    self.questionList = []
+    button1 = tk.Button(self, text="Select XML", command=lambda: UploadAction(self.questionList))
+    button1.pack()
 
-class SelectionPage(tk.Frame):
 
-    def __init__(self, questionList, parent, controller):
+##---------------------------------Question edition frame-------------------------------------
+class questionPage(tk.Frame):
+  isStarted = False
 
-        def UploadAction(questionList, event=None):
-            filename = filedialog.askopenfilename(filetypes=(("XML Files", "*.xml"),),title = "XML file selection", multiple = False)
-            print('Selected:', filename)
-            controller.setQuestionList(p.XMLparser(filename))
-            controller.changeNbQuest(len(questionList))
-            controller.show_frame("CorrectionPage")
+  ##-------to remove every widget from the page----
+  def clear_question(self):
+    widgetList = self.winfo_children()
+    for widget in widgetList:
+      widget.destroy()
 
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is the start page")
-        label.pack(side="top", fill="x", pady=10)
+  ##---------as if nothing ever happend--------------
+  def reset(self):
+    self.index = 0
+    self.clear_question()
+    self.controller.show_frame("uploadPage")
 
-        button1 = tk.Button(self, text="Select XML", command=lambda: UploadAction(questionList))
-        button1.pack()
+  ##---------just basic useful variables--------------
+  def __init__(self, parent, controller):
+    tk.Frame.__init__(self, parent)
+    self.controller = controller
+    self.index = 0
+    self.typeVariable = tk.StringVar()
 
-class CorrectionPage(tk.Frame):
+  ##----------show next question if any---------
+  def next(self):
+    if self.index < self.nbQuest - 1:
+      self.index += 1
+      self.clear_question()
+      self.startEdition()
+    else:
+      tk.messagebox.showinfo(title = None, message = "no more questions")
+      print("no more question")
 
-    def __init__(self, questionList, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is CorrectionPage")
-        label.pack(side="top", fill="x", pady=10)
+  ##----------show previous question if any---------
+  def previous(self):
+    if self.index > 0:
+      self.index -= 1
+      self.clear_question()
+      self.startEdition()
+    else:
+      tk.messagebox.showinfo(title = None, message ="no previous question")
+      print("already first question")
 
-        label = tk.Label(self, textvariable=question)
+  ##----------to get QuestionList from xml---------
+  def setQuestionList(self, questionList):
+    self.questionList = questionList
+    self.nbQuest = len(self.questionList)
 
-        button = tk.Button(self, text="Reset", command=lambda: controller.show_frame("SelectionPage"))
-        button.pack()
+  ##----------restrain entry box value to float between -100 100---------
+  def isInt(self, value_if_allowed):
+    try:
+      value = float(value_if_allowed)
+      return value >= -100 and value <= 100
+    except ValueError:
+      return value_if_allowed == ""
 
+  ##----------store inputed data---------
+  def save(self):
+    type = self.typeVariable.get()
+    if type == "Multichoice" or type == "SingleChoice":
+      newQuestion = p.questionClass("multichoice", self.questEntry.get())
+      if type == "Multichoice":
+        newQuestion.setSingle("false")
+      else: 
+        newQuestion.setSingle("true")
+    else:
+      newQuestion = p.questionClass("truefalse", self.questEntry.get())
+
+    if self.questEntry.get()=="":
+      tk.messagebox.showwarning(title="Warning", message="make sure all the informations are filled")
+      return
+    fbackList = []
+    for fback in self.feedbackTextList:
+      fbackList.append(fback.get())
+    general = fbackList[0]
+    correct = fbackList[1]
+    if type != "truefalse":
+      if len(fbackList) == 4:
+        partial = fbackList[2]
+        incorrect = fbackList[3]
+        newQuestion.setFeedback(general=general, correct=correct, partial=partial, incorrect=incorrect)
+      else:
+        incorrect = fbackList[2]
+        newQuestion.setFeedback(general=general, correct=correct, incorrect=incorrect)
+    elif type == "truefalse":
+      incorrect = fbackList[2]
+      newQuestion.setFeedback(general=general, correct=correct, incorrect=incorrect)
+    
+    for i in fbackList:
+      if i == "":
+        tk.messagebox.showwarning(title="Warning", message="make sure all the informations are filled")
+        return
+    for quest in self.answerTextList:
+      if quest[0].get()=="" or quest[1].get()=="":
+        tk.messagebox.showwarning(title="Warning", message="make sure all the informations are filled")
+        return
+      newQuestion.addAnswer(quest[0].get(), quest[1].get())
+    self.questionList[self.index] = newQuestion
+    self.clear_question()
+    self.startEdition()
+
+  ##----------delete the row j of answer---------
+  def deleteRow(self, j):
+    if len(self.answerTextList) > 2:
+      for w in self.answerTextList[j]:
+        w.destroy()
+      self.answerTextList.pop(j)
+    else:
+      print("at least 2 answers")
+
+  ##----------add a row of answer (save is necessary)---------
+  def addRow(self):
+    onlyInt = (self.register(self.isInt), '%P')
+    answerText = tk.Entry(self)
+    answerValue = tk.Entry(self, validate="key", validatecommand=onlyInt)
+    answerText.insert('end', "")
+    answerValue.insert('end', "")
+    self.answerTextList.append((answerText, answerValue))
+    self.save()
+
+  ##---------------------------------------------TEST----------------------------------
+  def deleteQuestion(self):
+    if self.nbQuest > 1:
+      self.nbQuest -= 1
+      self.questionList.pop(self.index)
+      while self.index >= self.nbQuest:
+        self.index -= 1
+      self.clear_question()
+      self.startEdition()
+
+  def addQuestion(self):
+    self.nbQuest += 1
+    defaultQuestion = p.questionClass("truefalse", "")
+    defaultQuestion.addAnswer("true", "100")
+    defaultQuestion.addAnswer("false", "0")
+    defaultQuestion.setFeedback()
+    self.questionList.insert(self.index, defaultQuestion)
+    self.clear_question()
+    self.startEdition()
+
+
+  ##--------------------final function to import question-------------
+  def checking(self):
+    ##TODO
+    
+
+    return self.questionList
+
+  def importQuest(self):
+    finalList = self.checking()
+    p.Upload(finalList)
+
+  ##-------initialise page at the current question index-------------------
+  def startEdition(self):
+    ##headers buttons
+    button = tk.Button(self, text="Reset", command=self.reset)
+    button.grid(row=0, column=1)
+    previousButton = tk.Button(self, text="previous", command=self.previous)
+    previousButton.grid(row=0, column=0)
+    nextButton = tk.Button(self, text="next", command=self.next)
+    nextButton.grid(row=0, column=2)
+    addQuestButton = tk.Button(self, text="add question", command=self.addQuestion)
+    addQuestButton.grid(row=0, column=3)
+    removeQuestButton = tk.Button(self, text="delete question", command=self.deleteQuestion)
+    removeQuestButton.grid(row=0, column=4)
+
+    ##---question----
+    if self.nbQuest > 0:
+
+      i = self.index
+      currentQuestion = self.questionList[i]
+
+      ##typeEntry = tk.Entry(self)
+      ##typeEntry.insert('end', self.questionList[i].getType())
+
+      ##here is type combobox
+      type = currentQuestion.getType()
+      self.typeCombo = ttk.Combobox(self, values=["Multichoice", "SingleChoice", "True/False"], state="readonly",
+                                    textvariable=self.typeVariable)
+      if type != "":
+        if type == "multichoice":
+          if currentQuestion.getSingle() == "true":
+            self.typeCombo.current(1)
+          elif currentQuestion.getSingle() == "false":
+            self.typeCombo.current(0)
+        elif type == "truefalse":
+          self.typeCombo.current(2)
+      typeText = tk.Label(self, text="type: ")
+
+      ##question text
+      self.questEntry = tk.Entry(self, width = 70)
+      self.questEntry.insert('end', currentQuestion.getQuestion())
+
+      questText = tk.Label(self, text="question: ")
+
+      typeText.grid(row=1, column=0)
+      ##typeEntry.pack(side="top", fill="x")
+      self.typeCombo.grid(row=1, column=1)
+      questText.grid(row=2, column=0)
+      self.questEntry.grid(row=2, column=1)
+
+      ##answers
+      answText = tk.Label(self, text="Answers: ")
+      answText.grid(row=3, column=0)
+      self.answerTextList = []
+
+      onlyInt = (self.register(self.isInt), '%P')
+      if type != "truefalse":
+        answers = currentQuestion.getAnswers()
+        for j in range(len(answers)):
+          answerText = tk.Entry(self, width = 70)
+          answerValue = tk.Entry(self,validate="key", validatecommand=onlyInt)
+          answerText.insert('end', answers[j][0])
+          answerValue.insert('end', answers[j][1])
+
+          answerText.grid(row=4 + j, column=0)
+          answerValue.grid(row=4 + j, column=1)
+          deleteAnswer = tk.Button(self, text="delete", command=lambda t=j: self.deleteRow(t))
+          deleteAnswer.grid(row=4 + j, column=2)
+          self.answerTextList.append((answerText, answerValue, deleteAnswer))
+      else:
+        answers = [("true", 100), ("false", 0)]
+        for j in range(2):
+          answerText = tk.Entry(self)
+          answerValue = tk.Entry(self)
+          answerText.insert('end', answers[j][0])
+          answerValue.insert('end', answers[j][1])
+          answerText.config(state='disabled')
+          answerValue.config(state='disabled')
+          answerText.grid(row=4 + j, column=0)
+          answerValue.grid(row=4 + j, column=1)
+          self.answerTextList.append((answerText, answerValue))
+
+      ##feedbacks
+      feedbackList = currentQuestion.getFeedback()
+      self.feedbackTextList = []
+      general = tk.Entry(self, width =50)
+      general.insert('end', feedbackList[0])
+      startfeedBack = 4 + len(answers)
+      feedbackLabel = tk.Label(self, text="Feedback:")
+      feedbackLabel.grid(row=startfeedBack, column=0)
+      general.grid(row=startfeedBack + 1, column=0)
+      self.feedbackTextList.append(general)
+      if type != "":
+        if type == "multichoice":
+          correct = tk.Entry(self, width =50)
+          partial = tk.Entry(self,width =50)
+          incorrect = tk.Entry(self,width =50)
+          correct.insert('end', feedbackList[1])
+          partial.insert('end', feedbackList[2])
+          incorrect.insert('end', feedbackList[3])
+          correct.grid(row=startfeedBack + 2, column=0)
+          partial.grid(row=startfeedBack + 3, column=0)
+          incorrect.grid(row=startfeedBack + 4, column=0)
+          next = startfeedBack + 5
+          self.feedbackTextList.append(correct)
+          self.feedbackTextList.append(partial)
+          self.feedbackTextList.append(incorrect)
+
+        elif type == "truefalse":
+          correct = tk.Entry(self)
+          incorrect = tk.Entry(self)
+          correct.insert('end', feedbackList[1])
+          incorrect.insert('end', feedbackList[3])
+          correct.grid(row=startfeedBack + 2)
+          incorrect.grid(row=startfeedBack + 3)
+          next = startfeedBack + 4
+          self.feedbackTextList.append(correct)
+          self.feedbackTextList.append(incorrect)
+
+      ##bottom buttons
+      saveButton = tk.Button(self, text="save", command=self.save)
+      saveButton.grid(row=25)
+      addAnsButton = tk.Button(self, text="add answer", command=self.addRow)
+      addAnsButton.grid(row=25, column=1)
+      finalButton = tk.Button(self, text="import question", command = self.importQuest)
+      finalButton.grid(row=25, column=2)
 if __name__ == "__main__":
-    app = visualParser()
-    app.mainloop()
+  app = visualParser()
+  app.mainloop()
